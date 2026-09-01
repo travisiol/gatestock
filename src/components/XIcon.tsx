92b983f1e363ecea4fd311ab365c0df49d13c441
travisiol <wowo27099@gmail.com>
2026-09-01 19:34:00 +0200

    Create XIcon.tsx

---
+++ src/components/XIcon.tsx
@@ -0,0 +1,13 @@
+export function XIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      aria-hidden="true"
+    >
+      <path d="M18.244 2H21.5l-7.5 8.57L22.5 22h-6.844l-5.36-6.99L4.2 22H.94l8.02-9.16L.5 2h7.02l4.84 6.39L18.244 2Zm-2.4 18h1.86L7.24 4H5.27l10.574 16Z" />
+    </svg>
+  );
+}
